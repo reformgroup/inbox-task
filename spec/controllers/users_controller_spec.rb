7b92ffd60cd31176b2ca7337230ed18afa401081
Nikolay Lipovtsev <n.lipovtsev@gmail.com>
2016-07-04 22:44:09 +0300

    Update user model

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -23,13 +23,8 @@ RSpec.describe UsersController, type: :controller do
   # This should return the minimal set of attributes required to create a valid
   # User. As you add validations to User, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:valid_attributes) { attributes_for(:user) }
+  let(:invalid_attributes) { attributes_for(:invalid_user) }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,66 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer          not null, primary key
+#  last_name           :string           not null
+#  first_name          :string           not null
+#  email               :string           not null
+#  gender              :integer          not null
+#  birth_date          :date             not null
+#  password_digest     :string           not null
+#  remember_digest     :string
+#  role                :integer          not null
+#  avatar_file_name    :string
+#  avatar_content_type :string
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  deleted_at          :datetime
+#  creator_id          :integer
+#  updater_id          :integer
+#  deleter_id          :integer
+#
+
 FactoryGirl.define do
   factory :user do
-    last_name "MyString"
-    first_name "MyString"
-    email "MyString"
-    gender 1
-    birth_date "2016-06-30"
-    password_digest "MyString"
-    remember_digest "MyString"
-    role 1
-    avatar ""
+    last_name { Faker::Name.last_name }
+    first_name { Faker::Name.first_name }
+    email { Faker::Internet.email }
+    birth_date { 18.years.ago - Faker::Number.number(3).to_i.days }
+    gender 'male'
+    password 'foobar'
+    password_confirmation 'foobar'
+    role 'user'
+    
+    factory :admin do
+      role 'admin'
+    end
+    
+    factory :superadmin do
+      role 'superadmin'
+    end
+    
+    factory :me do
+      last_name 'Константинопольский'
+      first_name 'Константин'
+      email 'test@mail.com'
+      gender 'male'
+      password '123123'
+      password_confirmation '123123'
+      role 'superadmin'
+      avatar { process_uri(Faker::Avatar.image) }
+    end
+    
+    factory :invalid_user do
+      last_name 'Константинопольский###'
+      first_name 'Константин_123'
+      email 'test@mail.com_ДА'
+      gender 'male_no'
+      password '123'
+      password_confirmation '123'
+      role 'superadmin_no'
+    end
   end
 end
